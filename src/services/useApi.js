576ac838e5d7434db33b8c98fe720d199e907744
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-12-09 15:46:53 -0500

    removed whitespace

--- src/services/useApi.js
+++ src/services/useApi.js
@@ -4,14 +4,10 @@ import {useState} from 'react'
 function useApi() {
     
     const apiKEY = process.env.REACT_APP_TMDB_API_KEY
-    
     const [searchTerm, setSearchTerm] = useState('')
     const [searchResultsMovie, setSearchResultsMovie] = useState([])
     const [searchResultsTv, setSearchResultsTv] = useState([])
     const [searchResultsPerson, setSearchResultsPerson] = useState([])
-
-    
-  
 
 
     const fetchSearchResults = async () => {
@@ -19,35 +15,24 @@ function useApi() {
             .then(res => res.json())
             .then((data) => {
                 
-                let results = data.results
-                
-                let tvSearchArr = [], movieSearchArr = [], personSearchArr = []
-                for (let i = 0; i < results.length; i++) {
-                    
+            let results = data.results
+            let tvSearchArr = [], movieSearchArr = [], personSearchArr = []
+                for (let i = 0; i < results.length; i++) {   
                 if (results[i].media_type === 'tv') {
-                    
                     tvSearchArr.push(results[i])
                 } else if (results[i].media_type === 'movie') {
                     movieSearchArr.push(results[i])
                 } else if (results[i].media_type === 'person') {
                     personSearchArr.push(results[i])
                 }
-            
-                
                 }
                 setSearchResultsMovie(movieSearchArr)
                 setSearchResultsTv(tvSearchArr)
                 setSearchResultsPerson(personSearchArr)
             }
-    
             )
             .catch(error => console.log(error))
             }  
-
-    
-       
-
-        
 
 
     return {fetchSearchResults, setSearchTerm, searchResultsMovie, searchResultsTv,  searchTerm, searchResultsPerson}
