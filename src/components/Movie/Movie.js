733818b5d8a7700103a9721efe962c5f9f4a9b33
SJellen <sjellen@me.com>
2020-12-13 14:21:32 -0500

    render and icon on Movie homepage link

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -29,6 +29,8 @@ function Movie() {
     const {buy, rent, flatrate} = movieProviders
 
     
+
+    
     
     
     
@@ -77,7 +79,8 @@ function Movie() {
         <div className="details-movie-details-container">
             {movieDetailsTile}
 
-            <span >{movieDetails.homepage}</span>
+         { movieDetails.homepage && movieDetails.homepage.length > 0  ?   <span className="movie-detail-homepage-link-container"><a href={movieDetails.homepage} target="_blank" rel="noopener noreferrer" className="movie-detail-homepage-link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-6 17c1.513-6.587 7-7.778 7-7.778v-2.222l5 4.425-5 4.464v-2.223c0 .001-3.78-.114-7 3.334z"/></svg></a></span> : ''}
+            
         </div>
             
         
