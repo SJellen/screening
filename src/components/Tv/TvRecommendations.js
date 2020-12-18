d89a4078f59772dafcdfb19654fd689e3bf8f80a
SJellen <sjellen@me.com>
2020-12-18 16:11:00 -0500

    tv similar and recommend ribbon

--- src/components/Tv/TvRecommendations.js
+++ src/components/Tv/TvRecommendations.js
@@ -1,12 +1,40 @@
-import {useContext} from 'react'
+import React, {useContext} from 'react'
 import {Context} from '../../Context'
-
+import blankSquare from '../../assets/placeholder.jpg'
+import {Link} from 'react-router-dom'
 
 
 
 function TvRecommendations() {
-    const {tvRecommendationArr, tvTileMaker}  = useContext(Context)
-    const tvRecommendTile = tvRecommendationArr && tvTileMaker(tvRecommendationArr)
+
+
+    const {posterPath, handleTvClick, tvRecommendationArr, handleRibbonTv, watchListTv, ratingTruncate, watchListTvIds, setWatchListTv}  = useContext(Context)
+
+    function handleRibbonMovieRecommendations(e,arr) {
+        let selection = arr[e].id
+        let selectionInfo = arr[e]
+        if (watchListTvIds.includes(selection)) {
+            setWatchListTv([...watchListTv.filter(item => item.id !== selection)])
+        } else {
+            setWatchListTv([...watchListTv, selectionInfo])
+        } 
+    } 
+ 
+
+    const tvRecommendTile = tvRecommendationArr && tvRecommendationArr.map((show, index) => 
+        
+    <div className="slider-card" key={show.id}> 
+    <span className="watchlist-ribbon">
+    { watchListTvIds.includes(show.id) ?
+         <i className="im im-bookmark im-bookmark-slider" onClick={() => handleRibbonMovieRecommendations(index, tvRecommendationArr)} style={{color: "#E1B517"}}></i> :
+         <i className="im im-bookmark im-bookmark-slider" onClick={() => handleRibbonMovieRecommendations(index, tvRecommendationArr)} style={{color: ""}}></i>
+       }
+        </span>
+    <Link to={`/tvPage/${show.id}`}><img  src={show.poster_path !== null ? `${posterPath}${show.poster_path}` : blankSquare} alt="poster" onClick={() => handleTvClick(index, tvRecommendationArr)}/></Link>
+    {ratingTruncate(show)}
+    <span className="slider-title" >{show.name}</span>
+    </div>
+    )
 
     return {tvRecommendTile}
 
--- src/components/Tv/TvSimilar.js
+++ src/components/Tv/TvSimilar.js
@@ -1,12 +1,39 @@
-import {useContext} from 'react'
+import React, {useContext} from 'react'
 import {Context} from '../../Context'
-
+import blankSquare from '../../assets/placeholder.jpg'
+import {Link} from 'react-router-dom'
 
 
 
 function TvSimilar() {
-    const {tvSimilarArr,tvTileMaker}  = useContext(Context)
-    const tvSimilarTile = tvSimilarArr && tvTileMaker(tvSimilarArr)
+
+    const {posterPath, handleTvClick, tvSimilarArr, handleRibbonTv, watchListTv, ratingTruncate, watchListTvIds, setWatchListTv}  = useContext(Context)
+
+    function handleRibbonTvSimilar(e,arr) {
+        let selection = arr[e].id
+        let selectionInfo = arr[e]
+        if (watchListTvIds.includes(selection)) {
+            setWatchListTv([...watchListTv.filter(item => item.id !== selection)])
+        } else {
+            setWatchListTv([...watchListTv, selectionInfo])
+        } 
+    } 
+
+
+    const tvSimilarTile = tvSimilarArr && tvSimilarArr.map((show, index) => 
+        
+    <div className="slider-card" key={show.id}> 
+    <span className="watchlist-ribbon">
+    { watchListTvIds.includes(show.id) ?
+         <i className="im im-bookmark im-bookmark-slider" onClick={() => handleRibbonTvSimilar(index, tvSimilarArr)} style={{color: "#E1B517"}}></i> :
+         <i className="im im-bookmark im-bookmark-slider" onClick={() => handleRibbonTvSimilar(index, tvSimilarArr)} style={{color: ""}}></i>
+       }
+        </span>
+    <Link to={`/tvPage/${show.id}`}><img  src={show.poster_path !== null ? `${posterPath}${show.poster_path}` : blankSquare} alt="poster" onClick={() => handleTvClick(index, tvSimilarArr)}/></Link>
+    {ratingTruncate(show)}
+    <span className="slider-title" >{show.name}</span>
+    </div>
+    )
 
     return {tvSimilarTile}
 
