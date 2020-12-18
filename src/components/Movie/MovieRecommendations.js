2e68224f943bf014d6b9647c7a228f08410679fb
SJellen <sjellen@me.com>
2020-12-18 16:02:50 -0500

    riboon on movie recommend and similar fixed

--- src/components/Movie/MovieRecommendations.js
+++ src/components/Movie/MovieRecommendations.js
@@ -1,14 +1,38 @@
-import {useContext} from 'react'
+import React, {useContext} from 'react'
 import {Context} from '../../Context'
-
+import blankSquare from '../../assets/placeholder.jpg'
+import {Link} from 'react-router-dom'
 
 
 
 function MovieRecommendations() {
 
-    const {movieRecommendationArr, movieTileMaker}  = useContext(Context)
+    const {posterPath, handleMovieClick, movieRecommendationArr, handleRibbonMovie, watchListMovie, ratingTruncate, watchListMovieIds, setWatchListMovie}  = useContext(Context)
 
-    const movieRecommendTile = movieRecommendationArr && movieTileMaker(movieRecommendationArr)
+    function handleRibbonMovieRecommendations(e,arr) {
+        let selection = arr[e].id
+        let selectionInfo = arr[e]
+        if (watchListMovieIds.includes(selection)) {
+            setWatchListMovie([...watchListMovie.filter(item => item.id !== selection)])
+        } else {
+            setWatchListMovie([...watchListMovie, selectionInfo])
+        } 
+    } 
+
+    const movieRecommendTile = movieRecommendationArr && movieRecommendationArr.map((movie, index) => 
+        
+    <div className="slider-card" key={movie.id}>
+    <span className="watchlist-ribbon">
+    {  watchListMovieIds && watchListMovieIds.includes(movie.id) ?
+         <i className="im im-bookmark im-bookmark-slider" onClick={() => handleRibbonMovieRecommendations(index, movieRecommendationArr)} style={{color: "#E1B517"}}></i> :
+         <i className="im im-bookmark im-bookmark-slider" onClick={() => handleRibbonMovieRecommendations(index, movieRecommendationArr)} style={{color: ""}}></i>
+       }
+        </span> 
+    <Link to={`/moviePage/${movie.id}`}><img  src={movie.poster_path !== null ? `${posterPath}${movie.poster_path}` : blankSquare} alt="poster" onClick={() => handleMovieClick(index, movieRecommendationArr, prevState => prevState)}/></Link>
+    {ratingTruncate(movie)}
+    <span className="slider-title" >{movie.title}</span>
+    </div>
+    )
 
     return {movieRecommendTile}
 
--- src/components/Movie/MovieSimilar.js
+++ src/components/Movie/MovieSimilar.js
@@ -1,14 +1,39 @@
-import {useContext} from 'react'
+import React, {useContext} from 'react'
 import {Context} from '../../Context'
-
+import blankSquare from '../../assets/placeholder.jpg'
+import {Link} from 'react-router-dom'
 
 
 
 function MovieSimilar() {
 
-    const {movieSimilarArr,movieTileMaker}  = useContext(Context)
+    const {posterPath, handleMovieClick,movieSimilarArr, handleRibbonMovie, watchListMovie, ratingTruncate, watchListMovieIds, setWatchListMovie}  = useContext(Context)
 
-    const movieSimilarTile = movieTileMaker(movieSimilarArr)
+    function handleRibbonMovieSimilar(e,arr) {
+        let selection = arr[e].id
+        let selectionInfo = arr[e]
+        if (watchListMovieIds.includes(selection)) {
+            setWatchListMovie([...watchListMovie.filter(item => item.id !== selection)])
+        } else {
+            setWatchListMovie([...watchListMovie, selectionInfo])
+        } 
+    } 
+
+    const movieSimilarTile = movieSimilarArr.map((movie, index) => 
+        
+    <div className="slider-card" key={movie.id}>
+    <span className="watchlist-ribbon">
+    { watchListMovieIds && watchListMovieIds.includes(movie.id) ?
+         <i className="im im-bookmark im-bookmark-slider" onClick={() => handleRibbonMovieSimilar(index, movieSimilarArr)} style={{color: "#E1B517"}}></i> :
+         <i className="im im-bookmark im-bookmark-slider" onClick={() => handleRibbonMovieSimilar(index, movieSimilarArr)} style={{color: ""}}></i>
+       }
+        </span> 
+    <Link to={`/moviePage/${movie.id}`}><img  src={movie.poster_path !== null ? `${posterPath}${movie.poster_path}` : blankSquare} alt="poster" onClick={() => handleMovieClick(index, movieSimilarArr, prevState => prevState)}/></Link>
+    {ratingTruncate(movie)}
+    <span className="slider-title" >{movie.title}</span>
+    </div>
+    )
+
     return {movieSimilarTile}
 
 
