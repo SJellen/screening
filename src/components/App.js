341bf0c73f9cacbd7ebf07a849180d41ddbed466
SJellen <sjellen@me.com>
2021-01-01 13:39:49 -0500

    clean up

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState
+  // , useEffect 
+} from 'react'
 import '../style/App.css';
 import '../style/MediaQueries.css'
 import Header from '../components/Header'
@@ -13,7 +15,7 @@ import MenuPage from './MenuPage'
 import MenuLinkPage from './MenuLinkPage'
 import SignIn from './SignIn/SignIn'
 import UserContext from '../UserContext'
-import Axios from 'axios'
+// import Axios from 'axios'
 import Home from './Home'
 
 
@@ -25,31 +27,31 @@ function App() {
     user: undefined
   })
 
-  useEffect(() => {
-    const checkedLoggedIn = async () => {
-      let token = localStorage.getItem("auth-token")
-        if (token === null) {
-         localStorage.setItem("auth-token", "")
-        token = "" 
-        } 
-      const tokenRes = await Axios.post(
-        "http://localhost:5000/users/tokenIsValid",
-        null, 
-        { headers: {"x-auth-token" : token} }
-      )
-      if (tokenRes.data) {
-        const userRes = await Axios.get("http://localhost:5000/users/", {
-          headers: { "x-auth-token": token}
-        })
-        setUserData({
-          user: userRes.data
-        })
-      }
-    }
+  // useEffect(() => {
+  //   const checkedLoggedIn = async () => {
+  //     let token = localStorage.getItem("auth-token")
+  //       if (token === null) {
+  //        localStorage.setItem("auth-token", "")
+  //       token = "" 
+  //       } 
+  //     const tokenRes = await Axios.post(
+  //       "http://localhost:5000/users/tokenIsValid",
+  //       null, 
+  //       { headers: {"x-auth-token" : token} }
+  //     )
+  //     if (tokenRes.data) {
+  //       const userRes = await Axios.get("http://localhost:5000/users/", {
+  //         headers: { "x-auth-token": token}
+  //       })
+  //       setUserData({
+  //         user: userRes.data
+  //       })
+  //     }
+  //   }
 
-    checkedLoggedIn() 
+  //   checkedLoggedIn() 
 
-  },[])
+  // },[])
 
 
   return (
